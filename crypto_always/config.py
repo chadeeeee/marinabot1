21c6150897ec77f8776f9bb1c48b9cd7a0f8527f
Chadee <esurginet2011@gmail.com>
2025-09-08 18:41:52 +0300

    dfdf

--- crypto_always/config.py
+++ crypto_always/config.py
@@ -4,14 +4,6 @@ TOKEN = "8136612723:AAGkO3LPdasADXO_8MknB5B4nVmfKTU5kjw"
 # Multiple userbot directories with their config imports
 USERBOT_DIRS = [
     "mybot_1",
-    "mybot_2",
     "mybot_3",
-    "mybot_4",
-    "mybot_5",
-    "mybot_6",
-    "mybot_7",
-    "mybot_8",
-    "mybot_9",
-    "mybot_10",
-    "mybot_11"
+    "mybot_8"
 ]
