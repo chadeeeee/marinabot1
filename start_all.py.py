53216e2dd7e12428beb9406f08377c22ffbda9c0
Chadee <esurginet2011@gmail.com>
2025-09-10 09:44:58 +0300

    вава

---
+++ start_all.py.py
@@ -0,0 +1,16 @@
+import subprocess
+
+bots = [
+    "mybot_1/main.py",
+    "mybot_3/main.py",
+    "mybot_8/main.py",
+    "crypto_always/main.py"
+]
+
+processes = []
+for bot in bots:
+    print(f"Запускаю {bot} ...")
+    processes.append(subprocess.Popen(["python", bot]))
+
+for p in processes:
+    p.wait()
